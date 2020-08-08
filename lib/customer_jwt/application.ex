bc4306793e8bcdd86165ba7eaeee5f45f024d0f6
Gavin Ballard <gavin@gavinballard.com>
2020-08-08 10:15:11 +1000

    Add :plug_cowboy

--- lib/customer_jwt/application.ex
+++ lib/customer_jwt/application.ex
@@ -4,16 +4,16 @@ defmodule CustomerJwt.Application do
   @moduledoc false
 
   use Application
+  require Logger
 
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: CustomerJwt.Worker.start_link(arg)
-      # {CustomerJwt.Worker, arg}
+      {Plug.Cowboy, scheme: :http, plug: CustomerJwt.TokenPlug, options: [port: 8080]}
     ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: CustomerJwt.Supervisor]
+
+    Logger.info("Starting application...")
+
     Supervisor.start_link(children, opts)
   end
 end
